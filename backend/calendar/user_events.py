a7243f4fad0d7d2e0835616e9172e48a626d3e9f
Charlie DeWitt <cdewitt24@utexas.edu>
2024-02-01 18:14:42 -0600

    small logistic changes

--- backend/calendar/user_events.py
+++ backend/calendar/user_events.py
@@ -11,24 +11,24 @@ from datetime import datetime
 def create_event(usr_id, event_id, created_epoch, event_desc, event_type, event_title, start_epoch, end_epoch, is_important, extra_data, is_submitted, want_notification, conn):
     cursor = conn.cursor()
     cursor.execute('''
-        INSERT INTO Calendar (usr_id, event_id, created_epoch, event_desc, event_type, event_title, start_epoch, end_epoch, is_important, extra_data, is_submitted, want_notification)
+        INSERT INTO calendar (usr_id, event_id, created_epoch, event_desc, event_type, event_title, start_epoch, end_epoch, is_important, extra_data, is_submitted, want_notification)
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     ''', (usr_id, event_id, created_epoch, event_desc, event_type, event_title, start_epoch, end_epoch, is_important, extra_data, is_submitted, want_notification))
     conn.commit()
 
 def read_event(event_id, conn):
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM Calendar WHERE event_id = ?', (event_id,))
+    cursor.execute('SELECT * FROM calendar WHERE event_id = ?', (event_id,))
     return cursor.fetchone()
 
 def update_event(event_id, new_event_title, conn):
     cursor = conn.cursor()
-    cursor.execute('UPDATE Calendar SET event_title = ? WHERE event_id = ?', (new_event_title, event_id))
+    cursor.execute('UPDATE clendar SET event_title = ? WHERE event_id = ?', (new_event_title, event_id))
     conn.commit()
 
 def delete_event(event_id, conn):
     cursor = conn.cursor()
-    cursor.execute('DELETE FROM Calendar WHERE event_id = ?', (event_id,))
+    cursor.execute('DELETE FROM calendar WHERE event_id = ?', (event_id,))
     conn.commit()
 
 

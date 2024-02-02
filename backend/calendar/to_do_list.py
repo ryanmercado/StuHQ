f4c5dc7b526528b1fdc2a6f99701caa20aa318f3
Charlie DeWitt <cdewitt24@utexas.edu>
2024-02-01 21:17:51 -0600

    to_do_list and changed connections

--- backend/calendar/to_do_list.py
+++ backend/calendar/to_do_list.py
@@ -1,29 +1,41 @@
+from datetime import datetime, timedelta
+import user_events
+import sqlite3
+
+
 class ToDoList():
+
     todo = [] # contains all the events to be shown in list
 
-    # Probably a flask end point 
-    def addEvent(self, event):
-        if(event.get("on_to_do_list", True)):
-           self.todo.append(event)
+    # NEED TO CHANGE IS_IMPORTANT 
 
-    def get_todo_list(self):
+    def get_todo_list(self, usr_id):
+        self.todo = self.getList(usr_id)
         return self.todo
+    
+    def add_to_list(self,usr_id, event_desc, event_type, event_title, start_epoch, end_epoch, is_important):
+        created_epoch = datetime.now().timestamp()
+        user_events.create_event(usr_id, 1, created_epoch, event_desc, event_type, event_title, start_epoch, end_epoch, is_important)
+        self.get_todo_list(usr_id)
 
+    def getList(self, usr_id):
+        con = sqlite3.connect('/home/cdew/StuHQ/server/usrDatabase/usrDB.db')
+        cursor = con.cursor()
+        next_week_datetime = datetime.now() + timedelta(days=7)
 
+        query = """
+            SELECT event_id, event_title, start_epoch
+            FROM calendar
+            WHERE usr_id = ? AND start_epoch >= ? AND start_epoch <= ? AND is_important = 1
+            ORDER BY start_epoch
+        """
+        now_epoch = datetime.now().timestamp()
+        next_week_epoch = next_week_datetime.timestamp()
 
-# Potential Flask configuration
+        cursor.execute(query, (usr_id, datetime.now(), next_week_epoch))
 
-# @app.route('/add_event', methods=['POST'])
-# def add_event():
-#     data = request.get_json()
-#     todo_list.add_event(data)
-#     return jsonify({'message': 'Event added successfully'})
+        result = cursor.fetchall()
 
-# @app.route('/get_todo_list', methods=['GET'])
-# def get_todo_list():
-#     return jsonify({'todo_list': todo_list.events})
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
+        cursor.close()
+        con.close()
 

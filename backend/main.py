6678d3a7d1bb87d6f7b92f380036401cb59a95e0
WilliamSterner <will1ster@utexas.edu>
2024-02-05 14:49:04 -0600

    created routes for flask app

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from recipe import RecipeBook, GroceryList
+from recipe import Recipe, GroceryList, Stock
 
 recipeAPI = Flask(__name__)
 
@@ -10,61 +10,92 @@ groceryList = GroceryList.GroceryList()
 @recipeAPI.route('/api/addRecipe', methods=['POST'])
 def create_recipe():
     data = request.get_json()
+    id = data['id']
     name = data['name']
     ingredients = data['ingredients']
+    measurements = data['measurements']
+    steps = data['steps']
 
-    recipe = RecipeBook.Recipe(name, ingredients)
-    recipeBook.add_recipe(recipe)
+    recipe = Recipe.Recipe(name, ingredients,measurements, steps)
+    recipes = [recipe]
+    Recipe.addRecipe(id, recipes)
     return jsonify({'message': 'Recipe created successfully'})
 
 
 @recipeAPI.route('/api/removeRecipe', methods=['POST'])
 def delete_recipe():
     data = request.get_json()
+    id = data['id']
     name = data['name']
     ingredients = data['ingredients']
+    measurements = data['measurements']
+    steps = data['steps']
 
-    recipe = RecipeBook.Recipe(name, ingredients)
-    recipeBook.remove_recipe(recipe)
+    recipe = Recipe.Recipe(name, ingredients, measurements, steps)
+    name = [recipe]
+    Recipe.removeRecipe(id, name)
     return jsonify({'message': 'Recipe deleted successfully'})
 
 
-@recipeAPI.route('/api/getRecipes', methods=['GET'])
+@recipeAPI.route('/api/getRecipes', methods=['POST'])
 def get_recipes():
-    recipes = recipeBook.get_recipes()
-    recipe_data = [{'name': recipe.get_name(), 'ingredients': recipe.get_ingredients()} for recipe in recipes]
-    return jsonify(recipe_data)
+    data = request.get_json()
+    id = data['id']
+    return Recipe.getRecipes(id)
+    
 
 
 @recipeAPI.route('/api/addIngredient', methods=['POST'])
 def add_ingredient():
     data = request.get_json()
-    ingredient = data['ingredient']
+    id = data['id']
+    item = data['item']
 
-    groceryList.add_item(ingredient)
+    GroceryList.add_item(id, item)
     return jsonify({'message': 'Ingredient added successfully'})
 
 
 @recipeAPI.route('/api/removeIngredient', methods=['POST'])
 def remove_ingredient():
     data = request.get_json()
-    ingredient = data['ingredient']
+    id = data['id']
+    item = data['item']
 
-    groceryList.remove_item(ingredient)
+    GroceryList.delete_item(id,item)
     return jsonify({'message': 'Ingredient deleted successfully'})
 
 
-@recipeAPI.route('/api/addRecipeIngredients', methods=['POST'])
-def add_recipe_ingredients():
+@recipeAPI.route('/api/getGroceryList', methods=['POST'])
+def getGroceryList():
     data = request.get_json()
-    name = data['name']
-    ingredients = data['ingredients']
+    id = data['id']
 
-    recipe = RecipeBook.Recipe(name, ingredients)
+    return GroceryList.get_items(id)
 
-    groceryList.add_recipe_ingredients(recipe)
-    return jsonify({'message': 'Ingredients added successfully'})
 
+@recipeAPI.route('/api/addStockItem', methods=['POST'])
+def addStockItem():
+    data = request.get_json()
+    id = data['id']
+    item = data['item']
+    Stock.add_item(id, item)
+    return jsonify({'message': 'Item added successfully'})
+
+
+@recipeAPI.route('/api/removeStockItem', methods=['POST'])
+def removeStockItem():
+    data = request.get_json()
+    id = data['id']
+    item = data['item']  
+    Stock.delete_item(id, item)
+    return jsonify({'message': 'Item removed successfully'})
+
+
+@recipeAPI.route('/api/getStock', methods=['POST'])
+def getStockItems():
+    data = request.get_json()
+    id = data['id']
+    return Stock.get_items(id)
 
 if __name__ == '__main__':
     recipeAPI.run(debug=True)

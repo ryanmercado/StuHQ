609b43edd76114b367656a8d7cf2bc73c71cb20a
ryanmercado <ryan.mercado@utexas.edu>
2024-02-05 12:02:05 -0600

    small change to dbInstall instructions

--- server/usrDatabase/dbInstall.sh
+++ server/usrDatabase/dbInstall.sh
@@ -6,3 +6,4 @@ sudo apt install sqlite3
 
 # to use db CLI, run 'sqlite3 usrDB.db'
 # ask GPT how to interact with SQLite3 CLI if you are lost. (.tables will show all our tables, PRAGMA table_info(tableName) will show all columns in a table, etc.)
+#PRAGMA table_info(tableName); to see table schema

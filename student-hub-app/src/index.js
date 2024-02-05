90c49801df7bf9beab42cee7424272a7bd976fa7
EmilianoC02 <emi.covarrubias@utexas.edu>
2024-02-05 15:40:35 -0600

    Create home, signup, and login page

--- student-hub-app/src/index.js
+++ student-hub-app/src/index.js
@@ -3,15 +3,21 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/index.css';
-import RecipeBookPage from './pages/RecipeBookPage'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import LogIn from './Home/LogIn';
+import SignUp from './Home/SignUp';
+import Landing from './Home/Landing';
 
 
 createRoot(document.getElementById('root')).render(
     <Router>
         <Routes>
-            <Route path = "/" element = {<RecipeBookPage />}/>
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/" element={<Landing />} />
+
+
         </Routes>
     </Router>
 );

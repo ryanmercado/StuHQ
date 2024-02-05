0ab22f44d8281e1447cceaf3fffd85dfa2476e86
Charlie DeWitt <cdewitt24@utexas.edu>
2024-02-05 16:05:08 -0600

    added comments/jsonified

--- backend/calendar/to_do_list.py
+++ backend/calendar/to_do_list.py
@@ -1,26 +1,51 @@
 from datetime import datetime, timedelta
+
+from flask import jsonify
 import user_events
+from check_ids import usr_id_exists, event_id_exists, UserNotFoundException, EventNotFoundException
 import sqlite3
 
 
-class ToDoList():
+def add_to_list(usr_id, event_desc, event_type, event_title, start_epoch, end_epoch, on_to_do_list):
 
-    todo = [] # contains all the events to be shown in list
+    '''
+        precondition: 
+            usr_id exists in db as INT, not NULL
+            event_desc is TEXT, not NULL
+            event_type is VARCHAR(50), not NULL
+            event_title is VARCHAR(50), not NULL
+            start_epoch is INT, can be NULL
+            end_epoch is INT, can be NULL
+            on_to_do_list is BOOLEAN, not NULL
 
-    def get_todo_list(self, usr_id):
-        self.todo = self.getList(usr_id)
-        return self.todo
-    
-    def add_to_list(self,usr_id, event_desc, event_type, event_title, start_epoch, end_epoch, on_to_do_list):
-        created_epoch = datetime.now().timestamp()
-        user_events.create_event(usr_id, 1, created_epoch, event_desc, event_type, event_title, start_epoch, end_epoch, on_to_do_list)
-        self.get_todo_list(usr_id)
+        postcondition:
+            creates a new event using create new event, setting the on_to_do_list value to true
 
-    def getList(self, usr_id):
-        con = sqlite3.connect('server/usrDatabase/usrDB.db')
-        cursor = con.cursor()
-        next_week_datetime = datetime.now() + timedelta(days=7)
+        TODO:
+            create event_id's so that they are unique
+            still needs to be tested with a unit test
 
+    '''
+
+    return user_events.create_event(usr_id, event_desc, event_type, event_title, start_epoch, end_epoch, on_to_do_list, None, None, None)
+
+def get_todo_list(usr_id):
+
+    '''
+        precondition: 
+            usr_id is present in the db as INT
+
+        postcondition: 
+            returns all of the events with on_to_do_list true and occuring in the next week
+
+        TODO:
+            still needs to be tested with a unit test
+    '''
+
+    con = sqlite3.connect('server/usrDatabase/usrDB.db')
+    cursor = con.cursor()
+    next_week_datetime = datetime.now() + timedelta(days=7)
+    if(usr_id_exists(usr_id)):
         query = """
             SELECT event_id, event_title, start_epoch
             FROM calendar
@@ -34,8 +59,11 @@ class ToDoList():
 
         result = cursor.fetchall()
 
-        self.todo = result
-
-        cursor.close()
-        con.close()
+        if result[0] > 0:
+            to_do_list = result[0] 
+            return jsonify({'to_do_list': to_do_list})
+        else:
+            return jsonify({'result': 'get to_do_list failed, events may be empty'})
+    else:
+        raise jsonify({'result': 'usr_id not found'})
 

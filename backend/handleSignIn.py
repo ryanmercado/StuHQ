d5e27bebd70a248c6be1e3f0a4a828cd073f7cc6
Grayson <grayson.drinkard@utexas.edu>
2024-02-06 14:17:46 -0600

    changed password to pswd_hash to match db

--- backend/handleSignIn.py
+++ backend/handleSignIn.py
@@ -6,7 +6,7 @@ def login(username, password):
     hash_pswd =handleCreateAccount.hash_password(username, password)
     conn = sqlite3.connect('server/usrDatabase/usrDB.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM usr_info WHERE username = ? AND password = ?', (username, hash_pswd))
+    cursor.execute('SELECT * FROM usr_info WHERE username = ? AND pswd_hash = ?', (username, hash_pswd))
     results = cursor.fetchone()
     if results[0] > 0: #user exists
         return jsonify({'result': 'log in successful'})

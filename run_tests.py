7c41da956de6bcd3e877fc8be4a82656c852fd9a
Grayson <grayson.drinkard@utexas.edu>
2024-02-10 20:35:11 -0600

    added comment to run specific file

--- run_tests.py
+++ run_tests.py
@@ -5,3 +5,8 @@ path_to_stuhq = sys.path[0]
 back_end_module = path_to_stuhq + '/backend'
 sys.path.append(back_end_module)
 pytest.main()
+
+# to run specific file: 
+# pytest.main(['backend/tests/test_registration_login.py'])
+
+

190a06a8f40f776cf8e9c1d9c25e335370f03746
WilliamSterner <will1ster@utexas.edu>
2024-02-12 14:02:55 -0600

    added routing for calendar backend functions

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from recipe import Recipe, GroceryList, Stock
+from recipe import Recipe, GroceryList, Stock, user_events, to_do_list
 
 recipeAPI = Flask(__name__)
 
@@ -88,5 +88,80 @@ def getStockItems():
     usr_id = data['usr_id']
     return Stock.get_items(usr_id)
 
+
+@calendarAPI.route('/api/addTo_ToDoList', methods=['POST'])
+def addTo_ToDoList():
+    data = request.get_json()
+    usr_id = data['usr_id']
+    event_desc = data['event_desc']
+    event_type = data['event_type']
+    event_title = data['event_title']
+    start_epoch = data['start_epoch']
+    end_epoch = data['end_epoch']
+    return to_do_list.add_to_list(usr_id, event_desc, event_type, event_title, start_epoch, end_epoch)
+
+
+@calendarAPI.route('/api/getToDoList', methods=['GET'])
+def get_ToDoList():
+    data = request.get_json()
+    usr_id = data['usr_id']
+    return to_do_list.get_todo_list(usr_id)
+
+
+@calendarAPI.route('/api/createEvent', methods=['POST']) 
+def createEvent():
+    data = request.get_json()
+    usr_id = data['usr_id']
+    event_desc = data['event_desc']
+    event_type = data['event_type']
+    event_title = data['event_title']
+    start_epoch = data['start_epoch']
+    end_epoch = data['end_epoch']
+    on_to_do_list = data['on_to_do_list']
+    extra_data = data['extra_data']
+    is_submitted = data['is_submitted']
+    want_notification = data['want_notification']
+    return user_events.create_event(usr_id, event_desc, event_type, event_title, start_epoch, end_epoch, on_to_do_list, extra_data, is_submitted, want_notification)
+
+
+@calendarAPI.route('/api/getEventInformation', methods=['GET']) 
+def getEventInformation():
+    data = request.get_json()
+    event_id = data['event_id']
+    return user_events.get_event(event_id)
+
+
+@calendarAPI.route('/api/getUserEvents', methods=['GET']) 
+def getUserEvents():
+    data = request.get_json()
+    usr_id = data['usr_id']
+    return user_events.get_usr_events(usr_id)
+
+
+@calendarAPI.route('/api/updateEvent', methods=['POST']) 
+def updateEvent():
+    data = request.get_json()
+    event_id = data['event_id']
+    event_desc = data['event_desc']
+    event_type = data['event_type']
+    event_title = data['event_title']
+    start_epoch = data['start_epoch']
+    end_epoch = data['end_epoch']
+    on_to_do_list = data['on_to_do_list']
+    extra_data = data['extra_data']
+    is_submitted = data['is_submitted']
+    want_notification = data['want_notification']
+    return user_events.update_event(event_id, event_title, event_desc, event_type, start_epoch, end_epoch, on_to_do_list, extra_data, is_submitted, want_notification) 
+
+
+@calendarAPI.route('/api/deleteEvent', methods=['POST']) 
+def deleteEvent():
+    data = request.get_json()
+    event_id = data['event_id']
+    return user_events.delte_event(event_id)
+
+
+
 if __name__ == '__main__':
     recipeAPI.run(debug=True)
+

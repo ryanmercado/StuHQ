2433d766cf86acde38e060875d1217cefb07c212
Grayson <grayson.drinkard@utexas.edu>
2024-02-13 14:06:22 -0600

    added test_recipe.py. It contains all tests for recipe backend. GroceryList.py, Recipe.py and Stock.py had bug fixes that the tests showed

--- run_tests.py
+++ run_tests.py
@@ -3,5 +3,8 @@ import sys
 
 path_to_stuhq = sys.path[0]
 back_end_module = path_to_stuhq + '/backend'
+recipe_module = back_end_module + '/recipe'
 sys.path.append(back_end_module)
-pytest.main()
+sys.path.append(recipe_module)
+# pytest.main()
+pytest.main(['backend/tests/test_recipe.py'])

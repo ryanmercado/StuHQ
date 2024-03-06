b6b62c5e0e44a16dc385e99bd03c4a5cef66ae3a
Charlie DeWitt <cdewitt24@utexas.edu>
2024-03-06 11:07:14 -0600

    Navbar can't appear on login/signup/home, usr_id verification fix

--- student-hub-app/src/App.js
+++ student-hub-app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation  } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/index.css';
 import reportWebVitals from './reportWebVitals';
@@ -10,7 +10,6 @@ import Landing from './Home/Landing';
 import ScrollPane from './Calendar/ScrollPane.js';
 import Dashboard from './Home/Dashboard.js'
 import ResumeDoc from './Resume/resumeDoc';
-
 import RecipeLanding from './Recipe/RecipeLanding.js';
 import RecipeList from './Recipe/RecipeList.js';
 import StockList from './Recipe/StockList.js';
@@ -19,27 +18,27 @@ import Navbar from './Navbar/Navbar.js';
 import secureLocalStorage from 'react-secure-storage';
 
 function App() {
-
-    secureLocalStorage.clear()
-
+    const location = useLocation();
+    const excludedRoutes = ['/signup', '/login', '/'];
+    const shouldRenderNavbar = !excludedRoutes.includes(location.pathname);
+    
     return (
         <>
-            <Navbar />
-            <Routes>
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/login" element={<LogIn />} />
-                <Route path="/" element={<Landing />} />
-                <Route path="/calendar" element={<ScrollPane />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/recipeLanding" element={<RecipeLanding />} />
-                <Route path="/resumeLanding" element={<ResumeDoc />} />
-                <Route path='/grocery-list' element={<GroceryList />} />
-                <Route path='/recipe-list' element={<RecipeList />} />
-                <Route path='/stock-list' element={<StockList />} />
-
-            </Routes>
+        {shouldRenderNavbar && <Navbar />}
+        <Routes>
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/" element={<Landing />} />
+            <Route path="/calendar" element={<ScrollPane />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/recipeLanding" element={<RecipeLanding />} />
+            <Route path="/resumeLanding" element={<ResumeDoc />} />
+            <Route path='/grocery-list' element={<GroceryList />} />
+            <Route path='/recipe-list' element={<RecipeList />} />
+            <Route path='/stock-list' element={<StockList />} />
+        </Routes>
         </>
-    )
+    );
 }
 
 export default App;
